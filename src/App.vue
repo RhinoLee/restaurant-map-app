c82d32b35caf9ad4a6d4d5aefa1234b248717905
RhinoLee <picklerhino@gmail.com>
2021-03-10 12:12:21 +0800

    add user location and find nearby restaurant

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Map></Map>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Map from './views/Map.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    Map
+  },
 }
 </script>
 
@@ -19,8 +19,14 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+.container {
+  height: 100%;
+}
+html,body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,8 @@ export default createStore({
   },
   mutations: {
   },
+  getters: {
+  },
   actions: {
   },
-  modules: {
-  }
 })
